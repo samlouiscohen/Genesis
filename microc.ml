91a67621a7cacbe7f0822c5e73261a87519f6d35
Sam Cohen <samsam489@gmail.com>
2017-11-17 16:57:16 -0500

    microc

---
+++ microc.ml
@@ -0,0 +1,28 @@
+(* Top-level of the MicroC compiler: scan & parse the input,
+   check the resulting AST, generate LLVM IR, and dump the module *)
+
+module StringMap = Map.Make(String)
+
+type action = Ast | LLVM_IR | Compile
+
+let _ =
+  let action = ref Compile in
+  let set_action a () = action := a in
+  let speclist = [
+    ("-a", Arg.Unit (set_action Ast), "Print the SAST");
+    ("-l", Arg.Unit (set_action LLVM_IR), "Print the generated LLVM IR");
+    ("-c", Arg.Unit (set_action Compile),
+      "Check and print the generated LLVM IR (default)");
+  ] in  
+  let usage_msg = "usage: ./microc.native [-a|-l|-c] [file.mc]" in
+  let channel = ref stdin in
+  Arg.parse speclist (fun filename -> channel := open_in filename) usage_msg;
+  let lexbuf = Lexing.from_channel !channel in
+  let ast = Parser.program Scanner.token lexbuf in
+  Semant.check ast;
+  match !action with
+    Ast -> print_string (Ast.string_of_program ast)
+  | LLVM_IR -> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
+  | Compile -> let m = Codegen.translate ast in
+    Llvm_analysis.assert_valid_module m;
+    print_string (Llvm.string_of_llmodule m)
