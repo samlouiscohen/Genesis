6f3749f29958f7c4b605348409eb6739a607a39b
Sam Cohen <samsam489@gmail.com>
2017-11-19 16:27:39 -0500

    Added float printing

--- codegen.ml
+++ codegen.ml
@@ -64,6 +64,7 @@ let translate (globals, functions) =
     let builder = L.builder_at_end context (L.entry_block the_function) in
 
     let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder in
+    let float_format_str = L.build_global_stringptr "%.6f\n" "fmt" builder in
     
     (* Construct the function's "locals": formal arguments and locally
        declared variables.  Allocate each on the stack, initialize their
@@ -118,6 +119,8 @@ let translate (globals, functions) =
           | A.Not     -> L.build_not) e' "tmp" builder
       | A.Assign (s, e) -> let e' = expr builder e in
 	                   ignore (L.build_store e' (lookup s) builder); e'
+      | A.Call ("printfl", [e]) -> L.build_call printf_func [| float_format_str ; (expr builder e) |]
+      "printf" builder 
       | A.Call ("print", [e]) | A.Call ("printb", [e]) ->
 	  L.build_call printf_func [| int_format_str ; (expr builder e) |]
 	    "printf" builder
