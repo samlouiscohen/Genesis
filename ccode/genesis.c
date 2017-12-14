c68ea9f791ad2077e5ed8f56e7b485ec01325967
Michael Wang <michael@dyn-160-39-235-228.dyn.columbia.edu>
2017-12-13 22:08:39 -0500

    added c code for key presses

--- ccode/genesis.c
+++ ccode/genesis.c
@@ -14,18 +14,25 @@ typedef struct position {
     int y;    
 } position;
 
+char *additionalKeynames[] = {"Up", "Down", "Left", "Right", "Space", "Escape"};
+
 SDL_Window *gWindow = NULL;
 SDL_Renderer *gRenderer = NULL;
 int backgroundR = 0xFF;
 int backgroundG = 0xFF;
 int backgroundB = 0xFF;
 int quit = 0;
+uint64_t downState = 0;
+uint64_t heldState = 0;
+uint64_t upState = 0;
 const Uint8 *keyStates = NULL;
 
 int initScreen(struct color *c, int width, int height);
 void clearScreen();
 void static close();
 void showDisplay();
+int keyToInt(char *keyName);
+uint64_t keyMask(int number);
 
 //Create screen
 int initScreen(struct color *c, int width, int height){
@@ -108,30 +115,86 @@ void static close(){
 
 void pollEvents(){
     SDL_Event event;
+    downState = 0;
+    upState = 0;
+    heldState = 0;
     while(SDL_PollEvent(&event)){
         if (event.type == SDL_QUIT){
             quit = 1;
-        } 
+        } else if (event.type == SDL_KEYDOWN){
+            const char* keyName = SDL_GetKeyName(event.key.keysym.sym);
+            uint64_t mask = keyMask(keyToInt(keyName));
+            if (event.key.repeat){
+                heldState |= mask;
+            } else {
+                downState |= mask;
+            }
+        } else if (event.type == SDL_KEYUP){
+            const char* keyName = SDL_GetKeyName(event.key.keysym.sym);
+            uint64_t mask = keyMask(keyToInt(keyName));
+            upState |= mask;
+        }
     }
     keyStates = SDL_GetKeyboardState(NULL);
 }
 
-int isKeyPressed(char key){
-    if (keyStates != NULL){
-        return keyStates[SDL_GetScancodeFromKey(key)];
+int keyInBitmask(uint64_t bitmask, char *keyName){
+    int keyInt = keyToInt(keyName);
+    return (bitmask & keyMask(keyInt)) != 0;
+}
+
+int isKeyHeld(char *key){
+    return keyInBitmask( heldState, key);
+}
+
+int isKeyDown(char *key){
+    return keyInBitmask( downState, key);
+}
+
+int isKeyUp(char *key){
+    return keyInBitmask( upState, key);
+}
+
+int keyToInt(char *keyName){
+    int numChars = 26;
+    int numInts = 10;
+    if (strlen(keyName) == 1){
+        //Check values for alphabet and number chars
+        char keyChar = keyName[0];
+        if(isalpha(keyChar)){
+            keyChar = toupper(keyChar);
+            return keyChar - 'A';
+        } else if (isdigit(keyChar)){
+            return numChars + keyChar - '0';
+        }
     } else {
-        return 0;
+        int i;
+        int numAdditionalKeys = sizeof(additionalKeynames) / sizeof(additionalKeynames[0]);
+        for(i = 0; i < numAdditionalKeys; i = i + 1){
+            if (strcmp(additionalKeynames[i], keyName) == 0){
+                return numChars + numInts + i;
+            }
+        }
     }
+}
+
+uint64_t keyMask(int number){
+    uint64_t mask = 1;
+    return mask << number;
 }
 
 void startGame(struct color *c, int width, int height){
     quit = 0;
     initScreen(c, width, height);
-    init();
+    // init();
     printf("%s\n", "Init successful");
     while (!quit){
-        update();
         pollEvents();
+        // update();
+
+        if(isKeyDown("Escape")){
+            printf("%s\n", "a was pressed");
+        }
     }
 }
 
@@ -149,22 +212,14 @@ void startGame(struct color *c, int width, int height){
 // }
 
 // #ifdef BUILD_TEST
-// int main(int argc, char* args[]){
-//     struct color col;
-//     col.r = 0xFF;
-//     col.g = 0xFF;
-//     col.b = 0xFF;
-//     //Make new screen
+int main(int argc, char* args[]){
+    struct color col;
+    col.r = 0xFF;
+    col.g = 0xFF;
+    col.b = 0xFF;
+    //Make new screen
 
-//     struct color *colptr = &col;
-//     initScreen(colptr, 640, 480);
-
-//     while(quit == 0){
-//         pollEvents();
-//         if(isKeyPressed('a')){
-//             printf("%s\n", "hello");
-//         }
-//     }
-//     close();
-// }
+    struct color *colptr = &col;
+    startGame(colptr, 640, 480);
+}
 // #endif
